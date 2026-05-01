9935b867322feb1d91aefb3ece4757b61ee62301
incosidium <sjkjdfjk@gmail.com>
2026-05-01 20:53:41 +0200

    Update score.js

--- js/score.js
+++ js/score.js
@@ -14,7 +14,7 @@ export function score(rank, percent, minPercent) {
     if (rank > 40) {
         return 0;
     }
-    if (rank > 20 && percent < 100) {
+    if (rank > 20&& percent < 100) {
         return 0;
     }
 
