f624f2bdd08045bfeb72e0ba38cfaeb34f4a5a95
incosidium <sjkjdfjk@gmail.com>
2026-06-16 14:58:42 +0200

    Update score function rank thresholds

--- js/score.js
+++ js/score.js
@@ -11,10 +11,10 @@ const scale = 3;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 40) {
+    if (rank > 80) {
         return 0;
     }
-    if (rank > 20&& percent < 100) {
+    if (rank > 30 && percent < 100) {
         return 0;
     }
 
